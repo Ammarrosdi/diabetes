1af76ba669619eef2057c617c5cfdfbaaadf09f6
Ammarrosdi <92770515+Ammarrosdi@users.noreply.github.com>
2022-04-04 13:35:28 +0800

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -3,22 +3,15 @@ import numpy as np
 import pandas as pd
 
 #import matplotlib.pyplot as plt
-from matplotlib import pyplot as plt
-from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
 from sklearn.metrics import classification_report
 
-
-
-st.title('Machine Learning - CLASSIFICATION')
+st.title('Heart Disease Predictor')
 
 st.sidebar.write("""
 This is a web app demo using python libraries such as Streamlit, Sklearn etc
@@ -26,152 +19,42 @@ This is a web app demo using python libraries such as Streamlit, Sklearn etc
 
 st.sidebar.write ("For more info, please contact:")
 
-st.sidebar.write("<a href='https://www.linkedin.com/in/yong-poh-yu/'>Dr. Yong Poh Yu </a>", unsafe_allow_html=True)
+st.sidebar.write("<a href='https://www.linkedin.com/in/mirulsraf/'>Amirul Asraf </a>", unsafe_allow_html=True)
 
+a = st.slider('Please insert your BMI',min_value= 15.0, max_value = 50.0, value=0.2)
+b = st.slider('Have you smoked at least 100 cigarettes in your entire life? [Note: 5 packs = 100 cigarettes] 1 for yes, 0 for no',min_value= 0, max_value = 1, value=1)
+c = st.slider('Do you a heavy drinker? (adult men having more than 14 drinks per week and adult women having more than 7 drinks per week) 1 for yes, 0 for no',min_value= 0, max_value = 1, value=1)
+d = st.slider('Do you have stroke? 1 for yes, 0 for no',min_value= 0, max_value = 1, value=1)
+e = st.slider('Now thinking about your physical health, which includes physical illness and injury, for how many days during the past 30 days was your physical health not good? (0-30 days)',min_value= 0, max_value = 30, value=1)
+f = st.slider('Thinking about your mental health, for how many days during the past 30 days was your mental health not good? (0-30 days)',min_value= 0, max_value = 30, value=1)
+g = st.slider('Do you have serious difficulty walking or climbing stairs? 1 for yes, 0 for no',min_value= 0, max_value = 1, value=1)
+h = st.slider('Do you have diabetes? 1 for yes, 0 for no',min_value= 0, max_value = 1, value=1)
+i = st.slider('Do you have exercise during the past 30 days other than your regular jobs? 1 for yes, 0 for no',min_value= 0, max_value = 1, value=1)
+j = st.slider('How you rate your general health? 1(bad) to 4(very good)',min_value= 0, max_value = 4, value=1)
+k = st.slider('How long do you sleep in hours every day?',min_value= 0, max_value = 12, value=1)
+l = st.slider('Do you have asthma? 1 for yes, 0 for no',min_value= 0, max_value = 1, value=1)
+m = st.slider('Do you have kidney disease? 1 for yes, 0 for no',min_value= 0, max_value = 1, value=1)
+n = st.slider('Do you have skin cancer? 1 for yes, 0 for no',min_value= 0, max_value = 1, value=1)
 
-choice = st.sidebar.radio(
-    "Choose a dataset",   
-    ('Default', 'User-defined '),
-    index = 0
-    
-)
+heart_data = pd.read_csv(r'https://raw.githubusercontent.com/mirulmio/HeartDisease/main/heart.csv')
 
-st.write(f"## You Have Selected <font color='Aquamarine'>{choice}</font> Dataset", unsafe_allow_html=True)
+import pandas as pd
 
-def get_default_dataset(name):
-    data = None
-    if name == 'Diabetes':
-        data = df = pd.read_csv (r'Path where the CSV file is stored\File name.csv')
-        print (df)
-   
-    X = data.data
-    y = data.target
-    return X, y
-
-def add_dataset_ui(choice_name):
-    X=[]
-    y=[]
-    X_names = []
-    X1 = []
-    if choice_name == 'Default':
-       dataset_name = st.sidebar.selectbox(
-            'Select Dataset',
-            ('Diabetes')
-        )
-       X, y = get_default_dataset (dataset_name)
-       X_names = X
-    else:
-        uploaded_file = st.sidebar.file_uploader(
-            "Upload a CSV",
-            type='csv'    )
-        
-
-        if uploaded_file!=None:
-           
-           st.write(uploaded_file)
-           data = pd.read_csv(uploaded_file)
-  
-        
-           y_name = st.sidebar.selectbox(
-                    'Select Label @ y variable',
-                    sorted(data)
-                    )
-
-           X_names = st.sidebar.multiselect(
-                     'Select Predictors @ X variables.',
-                     sorted(data),
-                     default = sorted(data)[1],
-                     help = "You may select more than one predictor"
-                     )
-
-           y = data.loc[:,y_name]
-           X = data.loc[:,X_names]
-           X1 = X.select_dtypes(include=['object'])
-        
-           X2 = X.select_dtypes(exclude=['object'])
-
-           if sorted(X1) != []:
-              X1 = X1.apply(LabelEncoder().fit_transform)
-              X = pd.concat([X2,X1],axis=1)
-
-           y = LabelEncoder().fit_transform(y)
-        else:
-           st.write(f"## <font color='Aquamarine'>Note: Please upload a CSV file to analyze the data.</font>", unsafe_allow_html=True)
-
-    return X,y, X_names, X1
-
-X, y , X_names, cat_var= add_dataset_ui (choice)
+diabetes = pd.read_csv('diabetes.csv')
 
 
 
 
-classifier_name = st.sidebar.selectbox(
-    'Select classifier',
-    ( 'Random Forest')
-)
-
-test_data_ratio = st.sidebar.slider('Select testing size or ratio', 
-                                    min_value= 0.10, 
-                                    max_value = 0.50,
-                                    value=0.2)
-random_state = st.sidebar.slider('Select random state', 1, 9999,value=1234)
-
-st.write("## 1: Summary (X variables)")
-
-
-if len(X)==0:
-   st.write("<font color='Aquamarine'>Note: Predictors @ X variables have not been selected.</font>", unsafe_allow_html=True)
-else:
-   st.write('Shape of predictors @ X variables :', X.shape)
-   st.write('Summary of predictors @ X variables:', pd.DataFrame(X).describe())
-
-st.write("## 2: Summary (y variable)")
-
-if len(y)==0:
-   st.write("<font color='Aquamarine'>Note: Label @ y variable has not been selected.</font>", unsafe_allow_html=True)
-elif len(np.unique(y)) <5:
-     st.write('Number of classes:', len(np.unique(y)))
-
-else: 
-   st.write("<font color='red'>Warning: System detects an unusual number of unique classes. Please make sure that the label @ y is a categorical variable. Ignore this warning message if you are sure that the y is a categorical variable.</font>", unsafe_allow_html=True)
-   st.write('Number of classes:', len(np.unique(y)))
-
-
-def add_parameter_ui(clf_name):
-    params = dict()
-    if clf_name == 'Random Forest':
-        
-        max_depth = st.sidebar.slider('max_depth', 2, 15,value=5)
-        params['max_depth'] = max_depth
-        n_estimators = st.sidebar.slider('n_estimators', 1, 100,value=10)
-        params['n_estimators'] = n_estimators
-    return params
-
-params = add_parameter_ui(classifier_name)
-
-def get_classifier(clf_name, params):
-    clf = None
-    if clf_name == 'Random Forest':
-       
-        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
-            max_depth=params['max_depth'], random_state=random_state)
-    return clf
-
-clf = get_classifier(classifier_name, params)
-
-
-st.write("## 3: Classification Report")
-
-if len(X)!=0 and len(y)!=0: 
 
 
 
-K = data.drop('Outcome',axis=1)
 
-l = data['Outcome']
+X = diabetes.drop('Outcome',axis=1)
+
+y = diabetes['Outcome']
 
 from sklearn.model_selection import train_test_split
-Xtrain, Xtest, ytrain, ytest = train_test_split(K, l,random_state=1234,test_size=0.2)
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y,random_state=1234,test_size=0.2)
 
 diabetes.info()
 
@@ -188,23 +71,14 @@ print()
 print()
 print(classification_report(ytest, ypred))
 
-  st.write('Classifier:',classifier_name)
-  st.write('Classification report:')
-  report = classification_report(y_test, y_pred,output_dict=True)
-  df = pd.DataFrame(report).transpose()
-  st.write(df)
+prediction(a,b,c,d,e,f,g,h,i,j,k,l,m,n)
 
-else: 
-   st.write("<font color='Aquamarine'>Note: No classification report generated.</font>", unsafe_allow_html=True)
-
-
-
-            
-            
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+def prediction(bmi, smoking, alcoholdrinking, stroke, physicalhealth, mentalhealth, diffwalking, diabetic, physicalactivity, genhealth, sleeptime, asthma, kidneydisease, skincancer):
+    heart_data2 = pd.DataFrame(columns = ['BMI', 'Smoking', 'AlcoholDrinking', 'Stroke', 'PhysicalHealth', 'MentalHealth', 'DiffWalking', 'Diabetic', 'PhysicalActivity',  'GenHealth', 'SleepTime', 'Asthma', 'KidneyDisease', 'SkinCancer' ])
+    heart_data2 = heart_data2.append({'BMI' : bmi, 'Smoking' : smoking, 'AlcoholDrinking' : alcoholdrinking, 'Stroke' : stroke, 'PhysicalHealth' : physicalhealth, 'MentalHealth' :mentalhealth, 'DiffWalking' :diffwalking, 'Diabetic' :diabetic, 'PhysicalActivity' :physicalactivity,  'GenHealth' :genhealth, 'SleepTime' :sleeptime, 'Asthma' :asthma, 'KidneyDisease' :kidneydisease, 'SkinCancer' :skincancer}, ignore_index = True) 
+    ypred = nb.predict(heart_data2)
+    st.write('Your prediction for have heart disease is:')
+    if ypred ==1:
+      st.write('Yes')
+    else:
+      st.write('No')  
